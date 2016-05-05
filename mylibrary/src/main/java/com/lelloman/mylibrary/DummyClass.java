ef38a1f145325b82e515617e16387b079c732f0e
lelloman <domenico.cerasuolo@mobgen.com>
2016-05-05 11:20:13 +0200

    DummyClass

---
+++ mylibrary/src/main/java/com/lelloman/mylibrary/DummyClass.java
@@ -0,0 +1,13 @@
+package com.lelloman.mylibrary;
+
+/**
+ * Created by lelloman on 05/05/16.
+ */
+public class DummyClass {
+
+	public final int value;
+
+	public DummyClass(int v){
+		value = v;
+	}
+}
